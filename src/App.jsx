58cb3a83667d36f0da081a43f9648dfc34e38d5c
log2-2 <ennichi824@gmail.com>
2023-10-29 17:33:02 +0900

    犬の画像を表示した。

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+// リポジトリをFork：
+// Forkすることで、元のリポジトリに変更を反映することなく自由に作業できる。
+
+// リポジトリをClone：
+// リポジトリはカレントディレクトリにダウンロードされる。
+
+// ページを開く：
+// yarn install
+// yarn dev
+
 // DO NOT DELETE
 
 import './App.css'
@@ -5,10 +15,17 @@ import './App.css'
 /**
  * @type {() => JSX.Element}
  */
+
 export const App = () => {
   return (
     <div>
-      <header>ヘッダ</header>
+      <header>Dog アプリ</header>
+      <p>犬の画像を表示するサイトです</p>
+      {/* 犬の画像を取得できるAPI「Dog API」 */}
+      <img
+        src={'https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1234.jpg'}
+        alt={'犬の画像'}
+      />
     </div>
   )
 }
